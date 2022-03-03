e1d274e95122bdd4f13b16004e372ac3b1757e7c
Grant Sherman <tnarg122@gmail.com>
2022-03-03 03:04:29 -0500

    Fully functional on screen keyboard with text input

--- src/components/key/index.jsx
+++ src/components/key/index.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { SpecialKey } from '../../pages/word-duel';
 import './key.css';
 
-export default function Key({ color, value }) {
-    if (value === '!') {
+export default function Key({ color, value, handleClick }) {
+    if (value === SpecialKey.Back) {
         value = "BACK"
         color = "big";
     }
-    if (value === '@') {
+
+    if (value === SpecialKey.Enter) {
         value = "ENTER"
         color = "big";
     }
+
     return (
-        <button className={"key " + color}>
+        <button className={"key " + color} onClick={() => handleClick(value)}>
             {value}
         </button>
     )
