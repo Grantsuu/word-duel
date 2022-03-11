1d838d745866f4d83e0794f5a80d5c9d3803babc
Grant Sherman <tnarg122@gmail.com>
2022-03-11 00:01:11 -0500

    Used quotations and added more descriptive test names

--- src/utils/index.test.js
+++ src/utils/index.test.js
@@ -4,45 +4,38 @@ const { EvaluationColor, getEvaluation, isMaxGuesses, isWin } = require('../util
  * getEvaluation
  */
 
-test('evaluations all match', () => {
+test("guess completely matches the word", () => {
     const word = "abcde";
     const guess = "abcde";
     const expected = [EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green];
     expect(getEvaluation(word, guess)).toEqual(expected);
 })
 
-test('no evaluations match', () => {
+test("no letters in guess match any in the word", () => {
     const word = "abcde";
     const guess = "fffff";
     const expected = [EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray];
     expect(getEvaluation(word, guess)).toEqual(expected);
 })
 
-test('one match', () => {
+test("exactly one letter in guess matches one letter in the word even with multiples in the guess", () => {
     const word = "abcde";
     const guess = "aaaaa";
     const expected = [EvaluationColor.Green, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray];
     expect(getEvaluation(word, guess)).toEqual(expected);
 })
 
-test('one partial', () => {
+test("exactly one letter in the guess partially matches one letter in the word even with multiples in the guess", () => {
     const word = "abcde";
     const guess = "faaaa";
     const expected = [EvaluationColor.Gray, EvaluationColor.Yellow, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray];
     expect(getEvaluation(word, guess)).toEqual(expected);
 })
 
-test('one match, one partial', () => {
+test("exactly one letter matches and exactly one letter partially matches even with multiples in the guess", () => {
     const word = "abbde";
     const guess = "aaaba";
     const expected = [EvaluationColor.Green, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Yellow, EvaluationColor.Gray];
-    expect(getEvaluation(word, guess)).toEqual(expected);
-})
-
-test('one match, no yellow overwrite', () => {
-    const word = "abbbb";
-    const guess = "aaccc";
-    const expected = [EvaluationColor.Green, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray];
     expect(getEvaluation(word, guess)).toEqual(expected);
 })
 
@@ -50,15 +43,19 @@ test('one match, no yellow overwrite', () => {
  * isMaxGuesses
  */
 
-test(`not at max guesses`, () => {
+test("empty array is not at max", () => {
     expect(isMaxGuesses([], 6)).toEqual(false);
 })
 
-test(`at max guesses`, () => {
+test("array with fewer arrays than the max is not at max", () => {
+    expect(isMaxGuesses([[], [], []], 6)).toEqual(false);
+})
+
+test("array containing exact amount of max arrays is at max", () => {
     expect(isMaxGuesses([[], [], [], [], [], []], 6)).toEqual(true);
 })
 
-test(`beyond max guesses`, () => {
+test("array containing more than the maximum amount of arrays is at max", () => {
     expect(isMaxGuesses([[], [], [], [], [], [], []], 6)).toEqual(true);
 })
 
@@ -66,18 +63,18 @@ test(`beyond max guesses`, () => {
  * isWin
  */
 
-test(`win`, () => {
+test("all green evaluations is a win", () => {
     expect(isWin([EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green])).toEqual(true);
 })
 
-test(`not win`, () => {
+test("a single not green evaluation is not a win", () => {
     expect(isWin([EvaluationColor.Gray, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green, EvaluationColor.Green])).toEqual(false);
 })
 
-test(`all yellow not win`, () => {
+test("all yellow evaluations is not win", () => {
     expect(isWin([EvaluationColor.Yellow, EvaluationColor.Yellow, EvaluationColor.Yellow, EvaluationColor.Yellow, EvaluationColor.Yellow])).toEqual(false);
 })
 
-test(`all gray not win`, () => {
+test("all gray evaluations is not win", () => {
     expect(isWin([EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray, EvaluationColor.Gray])).toEqual(false);
 })
